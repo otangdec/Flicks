ef77522df5eb4a1a957a593f7aaba4249ab6fa63
Oranuch Tangdechavut <finalehorizon@gmail.com>
2016-01-05 19:15:30 -0800

    - Add Progress Bar - Add search bar *incomplete*

--- Flicks/MoviesViewController.swift
+++ Flicks/MoviesViewController.swift
@@ -8,30 +8,39 @@
 
 import UIKit
 import AFNetworking // clean and build to make it work correctly
+import SVProgressHUD
 
-
-class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UITabBarDelegate, UISearchBarDelegate{
     
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var movieSearchBar: UISearchBar!
+
     
     var movies: [NSDictionary]? // optional can be dict or nil, safer
     
     var refreshControl: UIRefreshControl!
+    var filteredData: [NSDictionary]?
+    
 
     
     // Do any additional setup after loading the view.
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadMovies()
+        tableView.dataSource = self
+        tableView.delegate = self
+        movieSearchBar.delegate = self
+        
         
         self.refreshControl = UIRefreshControl()
         self.refreshControl.attributedTitle = NSAttributedString(string: "Pull to refresh")
         self.refreshControl.addTarget(self,action: "refresh:", forControlEvents: UIControlEvents.ValueChanged)
         self.tableView.addSubview(refreshControl)
+        
+        fetchMovieApi()
     }
     
     func refresh(sender:AnyObject){
-        loadMovies()
+        fetchMovieApi()
         self.refreshControl.endRefreshing()
     }
 
@@ -49,27 +58,31 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
     
-    func loadMovies(){
-        tableView.dataSource = self
-        tableView.delegate = self
+    func fetchMovieApi(){
         
         let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
         let url = NSURL(string:"https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
         let request = NSURLRequest(URL: url!)
+            
+            
         let session = NSURLSession(
             configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
             delegate:nil,
             delegateQueue:NSOperationQueue.mainQueue()
         )
         
+        //self.delay(3.0){ SVProgressHUD.show() }
+        SVProgressHUD.show()
         let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
             completionHandler: { (dataOrNil, response, error) in
+                //self.delay(5.0) { SVProgressHUD.dismiss() }
+                SVProgressHUD.dismiss()
                 if let data = dataOrNil {
                     if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
                         data, options:[]) as? NSDictionary {
                             print("response: \(responseDictionary)")
                             
-                            self.movies = responseDictionary["results"] as! [NSDictionary]
+                            self.movies = responseDictionary["results"] as? [NSDictionary]
                             self.tableView.reloadData()
                     }
                 }
@@ -77,6 +90,16 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         task.resume()
     }
     
+//    //add delay
+//    func delay(delay:Double, closure: () -> ()) {
+//        dispatch_after(
+//            dispatch_time(
+//                DISPATCH_TIME_NOW,
+//                Int64(delay * Double(NSEC_PER_SEC))
+//            ),
+//            dispatch_get_main_queue(), closure
+//        )
+//    }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     
@@ -103,14 +126,40 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         cell.posterView.setImageWithURL(imageUrl!)
         cell.titleLabel.text = title
         cell.overviewLabel.text = overview
-
-        
+    
      
         print("row \(indexPath.row)")
+    
+        
         return cell
     }
     
-
+    
+    func hideKeyboard(searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+    }
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        if(searchText.isEmpty) {
+            movies = movies!
+            dispatch_after(1, dispatch_get_main_queue()) { () -> Void in
+                self.hideKeyboard(searchBar)
+            }
+        } else {
+            movies = movies!.filter {
+                var name = $0["title"] as! String
+                return name.rangeOfString(searchText, options: .RegularExpressionSearch) != nil
+            }
+        }
+        
+        tableView.reloadData()
+    }
+    
+    func searchBarTextDidEndEditing(searchBar: UISearchBar){
+        fetchMovieApi()
+        tableView.reloadData()
+    }
+    
     /*
     // MARK: - Navigation
 
